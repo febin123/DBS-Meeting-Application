4c76805202c85445a1648aa7eb8d11997c702a5c
Febin Skariya <31155921+febin123@users.noreply.github.com>
2022-11-06 08:22:15 +0530

    Added MeetingService for listing all the meetings

---
+++ src/main/java/net/javaguides/springboot/service/MeetingService.java
@@ -0,0 +1,9 @@
+package net.javaguides.springboot.service;
+
+import java.util.List;
+
+import net.javaguides.springboot.model.Meeting;
+
+public interface MeetingService {
+List<Meeting> getAllMeeting();
+}
