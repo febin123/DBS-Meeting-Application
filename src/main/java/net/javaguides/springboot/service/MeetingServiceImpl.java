b86d02499ba64d7eec148e40a16553c46048c9e4
Febin Skariya <31155921+febin123@users.noreply.github.com>
2022-11-07 05:38:36 +0530

    Added service packages

--- src/main/java/net/javaguides/springboot/service/MeetingServiceImpl.java
+++ src/main/java/net/javaguides/springboot/service/MeetingServiceImpl.java
@@ -3,10 +3,12 @@ package net.javaguides.springboot.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import net.javaguides.springboot.model.Meeting;
 import net.javaguides.springboot.repository.MeetingRepository;
 
+@Service
 public class MeetingServiceImpl implements MeetingService {
 
 	@Autowired
