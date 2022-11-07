51e4f16602e4b2462de7d9fc727bbd6dc12ee402
Febin Skariya <31155921+febin123@users.noreply.github.com>
2022-11-08 01:18:12 +0530

    created delete method

--- src/main/java/net/javaguides/springboot/service/MeetingService.java
+++ src/main/java/net/javaguides/springboot/service/MeetingService.java
@@ -1,12 +1,10 @@
 package net.javaguides.springboot.service;
-
 import java.util.List;
-
 import net.javaguides.springboot.model.Meeting;
 
 public interface MeetingService {
 List<Meeting> getAllMeeting();
   void saveMeeting(Meeting meeting);
-  
   Meeting getMeetingId(long id);
+  void deleteMeetingById(long id);
 }
